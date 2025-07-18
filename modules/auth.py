37bc12bbe0440ddbd965f7f3b09256325644c25c
Diya Kumari Gupta <DiyaGupta@my.unt.edu>
2025-07-18 17:40:02 -0500

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -2,12 +2,14 @@ import streamlit as st
 from supabase import create_client
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
 
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# Debug print (remove after testing)
+print("SUPABASE_URL:", SUPABASE_URL)
+print("SUPABASE_KEY:", SUPABASE_KEY)
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
