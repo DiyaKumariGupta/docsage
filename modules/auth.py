195c67e46e88f719a37448695d043d2e6b9ba92c
Diya Kumari Gupta <DiyaGupta@my.unt.edu>
2025-07-18 17:31:53 -0500

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -2,6 +2,10 @@ import streamlit as st
 from supabase import create_client
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
